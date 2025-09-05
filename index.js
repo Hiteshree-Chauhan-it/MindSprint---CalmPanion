ef10af9ccc8695dbaee47c319568351648d72c68
HiteshreeChauhan <hiteshree08chauhan@gmail.com>
2025-09-05 18:28:32 +0530

    some stuff

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// const toggleBtn = document.getElementById('mode-toggle');
+// toggleBtn.addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode');
+//     toggleBtn.textContent = document.body.classList.contains('dark-mode') ? "☀️" : "🌙";
+// });
 const toggleBtn = document.getElementById('mode-toggle');
+const logo = document.getElementById('logo'); // your <img id="logo">
+
 toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-    toggleBtn.textContent = document.body.classList.contains('dark-mode') ? "☀️" : "🌙";
+
+    if (document.body.classList.contains('dark-mode')) {
+        toggleBtn.textContent = "☀️"; 
+        logo.src = "images/darkfinal.png";  // dark mode logo
+    } else {
+        toggleBtn.textContent = "🌙"; 
+        logo.src = "images/CalmPanionlogo.png"; // light mode logo
+    }
 });
